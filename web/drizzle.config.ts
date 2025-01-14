63a84b8550108c38aa26213914bbbdf56384a800
mikelmiras <mmiras004@ikasle.ehu.eus>
2025-01-15 00:54:14 +0100

    use different .env schema

--- web/drizzle.config.ts
+++ web/drizzle.config.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
+const URL = `postgres://postgres:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/kmm`
+
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: URL,
   },
 });
--- web/src/db/index.ts
+++ web/src/db/index.ts
@@ -2,7 +2,9 @@ import 'dotenv/config';
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
+const URL = `postgres://postgres:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/kmm`
+console.log("Connecting to database using URL: " + URL)
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: URL ,
 });
 export const db = drizzle({ client: pool });
