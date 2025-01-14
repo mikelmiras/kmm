6459de7f9fe008f9c93a267804b06124aa63bda6
mikelmiras <mmiras004@ikasle.ehu.eus>
2025-01-14 23:44:33 +0100

    Add gitignore

---
+++ web/src/db/schema.ts
@@ -0,0 +1,9 @@
+import { integer, pgTable, varchar, text } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  username: varchar({ length: 255 }).notNull().unique(),
+  password: text(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
