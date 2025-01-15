15e23ae185b1d2563b522c2e64afa68aaf14618e
mikelmiras <mmiras004@ikasle.ehu.eus>
2025-01-15 17:35:51 +0100

    latest

---
+++ file-handler/util/files.py
@@ -0,0 +1,37 @@
+import boto3
+from dotenv import *
+from botocore.config import Config
+import boto3, os
+load_dotenv()
+
+S3_URL = os.environ.get("S3_URL")
+S3_AUTH = os.environ.get("S3_AUTH_TOKEN")
+S3_SECRET = os.environ.get("S3_SECRET")
+S3_BUCKET = os.environ.get("S3_BUCKET")
+
+s3_client = boto3.client('s3',
+                         endpoint_url=S3_URL,
+                         aws_access_key_id=S3_AUTH,
+                         aws_secret_access_key=S3_SECRET,
+                         config=Config(signature_version='s3v4')
+                        )
+
+
+def uploadFolderToS3(folder:str, video_id:str):
+    folder_path = "uploads/{}".format(str(folder))
+
+    
+    with open(folder_path + "/master.m3u8", "w+") as f:
+        f.write("""#EXTM3U
+#EXT-X-STREAM-INF:BANDWIDTH=5000000,RESOLUTION=1920x1080
+1080p.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=3000000,RESOLUTION=1280x720
+720p.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=1000000,RESOLUTION=854x480
+480p.m3u8
+""")
+    filelist = os.listdir(folder_path)
+    for file in filelist:
+        full_file_path = "{}/{}".format(folder_path, file, video_id)
+        s3_client.upload_file(full_file_path, S3_BUCKET, video_id + "/" + file, ExtraArgs={'ACL': 'public-read'})
+    
