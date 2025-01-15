bff767c21879485377892225c6a2a842546e5e8c
mikelmiras <mmiras004@ikasle.ehu.eus>
2025-01-15 17:35:39 +0100

    Show player process

--- web/src/components/VideoPlayer.tsx
+++ web/src/components/VideoPlayer.tsx
@@ -2,10 +2,10 @@
 import { useEffect, useRef } from "react";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
-import "videojs-hls-quality-selector";
+import 'videojs-hls-quality-selector'
 
 const VideoPlayer = ({ streamingId }: { streamingId: string }) => {
-  const videoRef = useRef(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
   const playerRef = useRef(null);
 
   useEffect(() => {
@@ -13,51 +13,56 @@ const VideoPlayer = ({ streamingId }: { streamingId: string }) => {
 
     // Initialize the Video.js player
     const player = videojs(videoRef.current, {
-      autoplay: false, // Allow the user to control playback
-      controls: true,
-      preload: "auto",
-      fluid: true,
+      sources: [
+        {
+          src: `https://cdn.mikelm.dev/${streamingId}/master.m3u8`,
+          type: "application/x-mpegURL",
+        },
+      ],
     }) as any;
 
-    player.hlsQualitySelector({
-      displayCurrentQuality: true,
-    });
 
+    // Handle fullscreen change to unmute in fullscreen
     player.on("fullscreenchange", () => {
       if (player.isFullscreen()) {
         player.muted(false);
       }
     });
 
+
+    player.hlsQualitySelector({
+      displayCurrentQuality: true, // Display the current quality in the control bar
+    });
+
+
+    // Handle player errors
+    player.on("error", () => {
+      // Set a custom poster when there's an error
+      player.poster("/globe.svg");
+    });
+
+    
+
     playerRef.current = player;
 
     return () => {
       if (player) {
-        player.dispose(); // Dispose player on component unmount
+        player.dispose();
       }
     };
   }, [streamingId]);
 
   return (
     <div className="video-container">
-      <link
-        href="https://vjs.zencdn.net/8.16.1/video-js.css"
-        rel="stylesheet"
-      />
       <video
         ref={videoRef}
-        id="videoPlayer"
         className="video-js vjs-default-skin vjs-big-play-centered"
-        controls
-        preload="auto"
         width="640"
         height="360"
+        controls
+        preload="auto"
         data-setup="{}"
       >
-        <source
-          src={`https://streaming-eu.mikelm.dev/hls/test/${streamingId}/master.m3u8`}
-          type="application/x-mpegURL"
-        />
       </video>
     </div>
   );
